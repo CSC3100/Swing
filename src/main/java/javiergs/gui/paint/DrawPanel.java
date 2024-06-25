273dbc543746677596174986531b268015a2fed2
javiergs <3814755+javiergs@users.noreply.github.com>
2024-06-24 20:47:39 -0700

    Cleanning code

--- src/main/java/javiergs/gui/paint/DrawPanel.java
+++ src/main/java/javiergs/gui/paint/DrawPanel.java
@@ -1,96 +1,18 @@
-package paint;
+package javiergs.gui.paint;
 
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.util.Stack;
-import javax.swing.JPanel;
 
-public class DrawPanel extends JPanel implements MouseListener, MouseMotionListener {
-	
-	private Stack<Shape> shapes;
-	private String shape;
-	private String color;
-	private boolean drawing;
-	private int x, y, x2, y2;
+/**
+ * We will be able to draw in this place; wait for version 2.0.
+ *
+ * @author javiergs
+ * @version 1.0
+ */
+public class DrawPanel extends JPanel {
 	
 	public DrawPanel() {
-		addMouseListener(this);
-		addMouseMotionListener(this);
-		shapes = new Stack<Shape>();
-		drawing=false;
+		setBackground(new Color(176, 250, 192));
 	}
-	
-	public void setShape(String shape) {
-		this.shape = shape;
-	}
-	
-	public void setColor(String color) {
-		this.color = color;
-	}
-	
-	public void undo() {
-		if(!shapes.isEmpty()) shapes.pop();
-		repaint();
-	}
-	
-	public void erase() {
-		if(!shapes.isEmpty()) shapes.clear();
-		repaint();
-	}
-	
-	public void paintComponent (Graphics g) {
-		setBackground(Color.LIGHT_GRAY);
-		super.paintComponent(g);
-		if(shapes!=null) {
-			for(Shape shape: shapes) shape.draw(g);
-		}
-		g.setColor(Color.GRAY);
-		if (drawing)
-			g.drawRect(
-				(x2>x)?x:x2,
-				(y2>y)?y:y2,
-				(x2>x)?x2-x:x-x2,
-				(y2>y)?y2-y:y-y2
-			);
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		drawing=true;
-		x=e.getX();
-		y=e.getY();
-	}
-	
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		drawing=false;
-		x2 = e.getX();
-		y2 = e.getY();
-		if(shape.equals("paint.Rectangle")) {shapes.push(new Rectangle(x,y,x2,y2,color)); }
-		if(shape.equals("paint.Circle")) {shapes.push(new Circle(x,y,x2,y2,color)); }
-		if(shape.equals("paint.Arc")) {shapes.push(new Arc(x,y,x2,y2,color)); }
-		repaint();
-	}
-	
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		x2=e.getX();
-		y2=e.getY();
-		repaint();
-	}
-	
-	@Override
-	public void mouseEntered(MouseEvent e) {}
-	
-	@Override
-	public void mouseExited(MouseEvent e) {}
-	
-	@Override
-	public void mouseMoved(MouseEvent e) {}
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {}
 	
 }
--- src/main/java/javiergs/gui/paint/StatusPanel.java
+++ src/main/java/javiergs/gui/paint/StatusPanel.java
@@ -1,13 +1,20 @@
-package paint;
+package javiergs.gui.paint;
 
 import javax.swing.*;
 
+/**
+ * StatusPanel creates a panel with two buttons: Undo and Erase.
+ *
+ * @author javiergs
+ * @version 1.0
+ */
 public class StatusPanel extends JPanel {
-			public StatusPanel() {
-				// add two buttons to the panel
-				JButton button1 = new JButton("Undo");
-				JButton button2 = new JButton("Erase");
-				add(button1);
-				add(button2);
-		}
+	
+	public StatusPanel() {
+		JButton button1 = new JButton("Undo");
+		JButton button2 = new JButton("Erase");
+		add(button1);
+		add(button2);
+	}
+
 }
